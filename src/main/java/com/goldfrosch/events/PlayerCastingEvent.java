5797826ca48094e3f8aea029c4a5735883ce9522
goldfrosch <goldfrosch@daum.net>
2022-04-17 01:23:06 +0900

    GoldFrosch / feat slot auto consume
    1. feat change slot auto consume mmoitems
    2. refactor use Optional
    3. feat add MythicLibs

--- src/main/java/com/goldfrosch/events/PlayerCastingEvent.java
+++ src/main/java/com/goldfrosch/events/PlayerCastingEvent.java
@@ -1,13 +1,17 @@
 package com.goldfrosch.events;
 
 import com.goldfrosch.MMOBridge;
+import io.lumine.mythic.lib.api.item.NBTItem;
+import net.Indyuce.mmoitems.api.interaction.Consumable;
+import net.Indyuce.mmoitems.api.interaction.UseItem;
 import lombok.RequiredArgsConstructor;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class PlayerCastingEvent implements Listener {
@@ -18,17 +22,21 @@ public class PlayerCastingEvent implements Listener {
         Player player = e.getPlayer();
         ItemStack[] invList = player.getInventory().getContents();
 
-        if(invList[e.getNewSlot()] != null) {
-            String getItemData = String.valueOf(
-                invList[e.getNewSlot()].
-                getItemMeta().getAttributeModifiers(Attribute.GENERIC_ATTACK_SPEED)
-            );
+        Optional.ofNullable(invList[e.getNewSlot()]).ifPresent(selectItem -> {
+            Optional.ofNullable(NBTItem.get(selectItem)).ifPresent(nbtItem -> {
+                UseItem useItem = UseItem.getItem(player, nbtItem, nbtItem.getType());
 
-            if(!getItemData.isEmpty()) {
-                if(getItemData.contains("name=mmoitemsDecoy")) {
-                    player.sendMessage("테스트 완료");
+                if(useItem instanceof Consumable) {
+                    Consumable.ConsumableConsumeResult result = ((Consumable) useItem).useOnPlayer(player.getHandRaised(), false);
+                    player.sendMessage(String.valueOf(result));
+                    if (result == Consumable.ConsumableConsumeResult.CONSUME) {
+                        selectItem.setAmount(selectItem.getAmount() - 1);
+                    }
                 }
-            }
-        }
+
+                useItem.getPlayerData().applyItemCooldown(useItem.getMMOItem(), useItem.getNBTItem().getStat("ITEM_COOLDOWN"));
+                useItem.executeCommands();
+            });
+        });
     }
 }
