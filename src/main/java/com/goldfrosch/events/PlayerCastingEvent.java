56c0ba769375900d976c33d3383cbdb62a03bdba
이상민 <goldfrosch@daum.net>
2022-04-17 21:29:52 +0900

    GoldFrosch / feat Casting Consume
    1. feat consume mmoitem if player is skill casting scroll inventory and auto consume item! LOL!

--- src/main/java/com/goldfrosch/events/PlayerCastingEvent.java
+++ src/main/java/com/goldfrosch/events/PlayerCastingEvent.java
@@ -2,6 +2,7 @@ package com.goldfrosch.events;
 
 import com.goldfrosch.MMOBridge;
 import io.lumine.mythic.lib.api.item.NBTItem;
+import net.Indyuce.mmocore.api.player.PlayerData;
 import net.Indyuce.mmoitems.api.interaction.Consumable;
 import net.Indyuce.mmoitems.api.interaction.UseItem;
 import lombok.RequiredArgsConstructor;
@@ -20,25 +21,29 @@ public class PlayerCastingEvent implements Listener {
     @EventHandler
     public void onPlayerChangeHotSlotIfCasting(PlayerItemHeldEvent e) {
         Player player = e.getPlayer();
-        ItemStack[] invList = player.getInventory().getContents();
+        PlayerData playerData = PlayerData.get(player);
 
-        Optional.ofNullable(invList[e.getNewSlot()]).ifPresent(selectItem -> {
-            NBTItem nbtItem = NBTItem.get(selectItem);
-            if(nbtItem.getType() != null) {
-                UseItem useItem = UseItem.getItem(player, nbtItem, nbtItem.getType());
+        if(playerData.isCasting() && e.getNewSlot() >= 5 && e.getNewSlot() <= 8) {
+            ItemStack[] invList = player.getInventory().getContents();
 
-                if(useItem instanceof Consumable) {
-                    if (useItem.checkItemRequirements()) {
-                        Consumable.ConsumableConsumeResult result = ((Consumable) useItem).useOnPlayer(player.getHandRaised(), false);
-                        if (result == Consumable.ConsumableConsumeResult.CONSUME) {
-                            selectItem.setAmount(selectItem.getAmount() - 1);
+            Optional.ofNullable(invList[e.getNewSlot()]).ifPresent(selectItem -> {
+                NBTItem nbtItem = NBTItem.get(selectItem);
+                if(nbtItem.getType() != null) {
+                    UseItem useItem = UseItem.getItem(player, nbtItem, nbtItem.getType());
+
+                    if(useItem instanceof Consumable) {
+                        if (useItem.checkItemRequirements()) {
+                            Consumable.ConsumableConsumeResult result = ((Consumable) useItem).useOnPlayer(player.getHandRaised(), false);
+                            if (result == Consumable.ConsumableConsumeResult.CONSUME) {
+                                selectItem.setAmount(selectItem.getAmount() - 1);
+                            }
                         }
                     }
-                }
 
-                useItem.getPlayerData().applyItemCooldown(useItem.getMMOItem(), useItem.getNBTItem().getStat("ITEM_COOLDOWN"));
-                useItem.executeCommands();
-            }
-        });
+                    useItem.getPlayerData().applyItemCooldown(useItem.getMMOItem(), useItem.getNBTItem().getStat("ITEM_COOLDOWN"));
+                    useItem.executeCommands();
+                }
+            });
+        }
     }
 }
